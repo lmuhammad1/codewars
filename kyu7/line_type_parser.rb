dd391a63dfd7961a4f416f444a36342d8b955e12
Louis Muhammad <louis.muhammad@gmail.com>
2014-01-22 08:18:42 -0500

    Line type parser

---
+++ kyu7/line_type_parser.rb
@@ -0,0 +1,17 @@
+def line_types lines
+	final_array = []
+	lines.each do |line|
+		x = line.to_s.downcase
+		if x.include? "alpha"
+			final_array << :alpha
+		elsif x.include? "beta"
+			final_array << :beta
+		else
+			final_array << :unknown
+		end
+	end
+	final_array
+
+end
+
+p line_types ["I'm Alpha", "No idea", "beta tester", nil]
